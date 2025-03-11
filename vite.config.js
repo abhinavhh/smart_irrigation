7a76e89e4e85b4823d281562e23de075da428fc1
Abhinav <abhinavponnu964531@gmail.com>
2025-03-11 23:21:57 +0530

    edited config file

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,15 @@ export default defineConfig({
     historyApiFallback: true
   },
   build: {
-      outDir: 'dist'
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
   },
   base: '/',
   resolve: {
