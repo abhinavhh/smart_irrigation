98e54a7a279cbdbe09abacc9552b59c4bd71c682
Abhinav <abhinavponnu964531@gmail.com>
2025-03-27 12:44:48 +0530

    configured path

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import image from "/Users/hp/Documents/React/projectS8/smart_irrigation/src/assets/untitled-5-o.avif";
+import image from "../assets/untitled-5-o.avif";
 import {
   ChartBarIcon,
   PlusCircleIcon,
